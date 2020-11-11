45fa1e8f963539b6b0be2bf79ee5da7c8babd383
Daniel Sasse <dsasse07@gmail.com>
2020-11-11 20:12:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
 # Write your methods here
+
+def loop_message_five_times (string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times (string, times)
+  counter = 1 
+  while counter <= times
+    puts string
+    counter +=1 
+  end
+end
+
+def output_array (array)
+  for array.each |i| do
+    puts i
+end
+
+# def return_string_array (array)
+#     array.collect |i| {i.to_s}
+#   end
+  
